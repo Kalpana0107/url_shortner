d100b38df21376e76e1037d9a93e692cb5a90212
Kalpana0107 <naikodikalpana07@gmail.com>
2026-09-04 20:37:24 +0530

    adding another route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/",(req,res)=>{
  res.send("Url Shortener Backend");
 });
+app.get('/',(req,res)=>{
+    res.send("Hello server is running!");
+});
 
 app.listen(5000,()=>{
     console.log("Congrats Server is running on port 5000");
