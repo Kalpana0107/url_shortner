da3345482939d76d3ee7d64b89d8d5b57e5cff96
Kalpana0107 <naikodikalpana07@gmail.com>
2026-09-05 19:16:58 +0530

    checking server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express= require("express");
 const app = express();
-
+app.use(express.json());
 app.get("/",(req,res)=>{
  res.send("Url Shortener Backend");
 });
-app.get('/',(req,res)=>{
-    res.send("Hello server is running!");
+app.post("/api/shorten", (req, res) => {
+  console.log(req.body);
+  res.send("got it");
 });
 
+app.post("/api/shorten", (req, res) => {
+  console.log("ROUTE WAS HIT");
+  console.log(req.body);
+  res.send("got it");
+});
 app.listen(5000,()=>{
     console.log("Congrats Server is running on port 5000");
 });
