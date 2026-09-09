46282d86d1800ce8fd4bc8e94b7dfdbd218a17e8
Kalpana0107 <naikodikalpana07@gmail.com>
2026-09-09 21:04:31 +0530

    Adding the json

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,12 @@ app.use(express.json());
 app.get("/",(req,res)=>{
  res.send("Url Shortener Backend");
 });
-app.post("/api/shorten", (req, res) => {
-  console.log(req.body);
-  res.send("got it");
-});
+
 
 app.post("/api/shorten", (req, res) => {
   console.log("ROUTE WAS HIT");
   console.log(req.body);
-  res.send("got it");
+  res.json({ received: req.body.url });
 });
 app.listen(5000,()=>{
     console.log("Congrats Server is running on port 5000");
